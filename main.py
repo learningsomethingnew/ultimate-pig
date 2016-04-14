cc3ed481b0b7ef365319001e2bd90d3a3df0febf
Nic Colley <nic.colley@live.com>
2016-04-14 01:47:05 -0400

    tracking stats and second player implemented

--- main.py
+++ main.py
@@ -2,42 +2,74 @@ from player import BasePlayer
 
 
 #####################
-# !Make game loop
+# Main class. Manages the number of turns and
+# the number of sims of the app.
 ####################
 
 class Main():
-    def __init__(self):
+    def __init__(self, num_of_sims, a_player1, a_player2):
         self.number_of_turns = 7
-        self.player1 = BasePlayer(a_name="Player_1")
-        self.player2 = BasePlayer(a_name="Player_2")
+        self.player1 = a_player1
+        self.player2 = a_player2
+
+        self.num_of_sims = num_of_sims
+
+    """Runs the game for the # of sims needed"""
+
+    def main(self):
+        for _ in range(self.num_of_sims):
+            self.game_loop()
 
     """Manages the 7 turns"""
 
-    def main_game_loop(self):
+    def game_loop(self):
+        self.player1.reset_total_score()
+        self.player2.reset_total_score()
         game_turns = 0
         while game_turns < self.number_of_turns:
             self.player1.decisions_for_each_turn()
             self.player2.decisions_for_each_turn()
             game_turns += 1
-        self.win_loose()
+        self.win_lose()
 
-    def win_loose(self):
+    def win_lose(self):
         # Player 1 Wins
-        if self.player1.get_total_points() > self.player2.get_total_points():
+        if self.player1.get_total_points() > \
+                self.player2.get_total_points():
             print("Player 1 Wins")
-            self.print_player_points()
+            self.game_results()
+            # track stats of player 1
+            self.player1.track_game_stats(1)
+            self.player2.track_game_stats()
         # Player 2 Wins
-        elif self.player1.get_total_points() < self.player2.get_total_points():
+        elif self.player1.get_total_points() < \
+                self.player2.get_total_points():
             print("Player 2 Wins")
-            self.print_player_points()
+            self.game_results()
+            # track stats of player 2
+            self.player1.track_game_stats()
+            self.player2.track_game_stats(1)
         else:
             print("It's a tie")
 
-    def print_player_points(self):
+    def game_results(self):
         print("Player 1 points = {}".format(self.player1.get_total_points()))
         print("Player 2 points = {}".format(self.player2.get_total_points()))
+        self.player2.track_game_stats()
 
 
 if __name__ == "__main__":
-    f = Main()
-    f.main_game_loop()
+
+    # Base vs Base
+    b_player1 = BasePlayer("Player1")
+    b_player2 = BasePlayer("Player2")
+    f = Main(1000, b_player1, b_player2)
+    f.main()
+
+
+
+    # Roll 5 Player vs Roll 5
+    r5_player1 = BasePlayer("Player1")
+    r5_player2 = BasePlayer("Player2")
+    g = Main(1000, r5_player1, r5_player2)
+
--- player.py
+++ player.py
@@ -1,87 +1,142 @@
 from random import randint
 
+
 class Player():
-    def __init__(self, a_name = ""):
+    def __init__(self, a_name=""):
         self.player_total_score = 0
         self.player_name = a_name
         self.player_bank_score = 0
+        # tracking stats
+        self.wins = 0
+        self.game_count = 0
+        self.dice_value_per_roll = []
+        self.bank_score_list = []
+        self.total_points_list = []
+
+    """Returns player name"""
+
+    def get_name(self):
+        return self.player_name
 
     """Decides if to roll or hold"""
+
     def choose_roll_hold(self):
         pass
 
-    """Set total points for player"""
-    def set_total_points(self, value):
-        self.player_total_score += value
+    """Set total points from bucket turn"""
+
+    def set_total_points(self, a_value):
+        self.player_total_score += a_value
 
     """Gets total points from player"""
+
     def get_total_points(self):
         return self.player_total_score
 
+    """Resets player score"""
+    def reset_total_score(self):
+        self.player_total_score = 0
+
     """During a turn, set the bank score"""
+
     def set_bank_score(self, value):
         self.player_bank_score = value
 
     """Returns the int of the player's in turn score"""
+
     def get_bank_score(self):
         return self.player_bank_score
 
     """Returns int for dice"""
+
     def get_dice_roll(self):
-        temp_dice =  randint(1,6)
-        print("{} rolled a {}.".format(self.player_name,    temp_dice))
+        temp_dice = randint(1, 6)
+        #STATS: tracks the dice roll
+        self.dice_value_per_roll.append(temp_dice)
         return temp_dice
 
     """Prints generic object information"""
+
     def __str__(self):
-        return("I'm {}".format(self.player_name))
+        return ("I'm {}".format(self.player_name))
 
     """Manages players action for turn"""
+
     def decisions_for_each_turn(self):
         player_turn = True
         self.set_bank_score(0)
 
-        while player_turn == True:
+        while player_turn:
             temp_dice = 0
             temp_choice = self.choose_roll_hold()
-
+            #player chooses to roll
             if temp_choice == 'roll':
+                #tracking stats
+                #getting the roll information
                 temp_dice = self.get_dice_roll()
+                #testing if it ends turn
                 if temp_dice == 1:
                     self.set_bank_score(0)
+                    #break the loop
                     player_turn = False
+                #if not, it adds the score to the bank
                 else:
                     self.set_bank_score(temp_dice)
+            #player chooses hold
             elif temp_choice == 'hold':
+                #adds bank to the total
                 self.set_total_points(self.get_bank_score())
+                #break loop
                 player_turn = False
 
+    """takes in the win state and tracks the data"""
 
+    def track_game_stats(self, a_win=0):
+        self.game_count += 1
+        self.wins += a_win
+        # STATS: passes the number of dice rolls
+        self.set_total_points_list(self.get_total_points())
+
+    """Returns game count and wins"""
+
+    def get_game_stats(self):
+        return (self.game_count, self.wins)
+
+    """Returns list of dice values per role"""
+
+    def get_dice_values_per_role(self):
+        return self.dice_value_per_roll
+
+    """Appends an int to the bank score list"""
+    def set_bank_score_list(self, a_value):
+        self.bank_score_list.append(a_value)
+
+    """Returns list of bank score per turn"""
+    def get_bank_score_list(self):
+        return self.bank_score_list
+
+    """Appends an int to the total points"""
+    def set_total_points_list(self, a_value):
+        self.total_points_list.append(a_value)
+
+    """Returns list of the total points list"""
+    def get_total_points_list(self):
+        return self.total_points_list
 
 
 class BasePlayer(Player):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    "Ensures Baseplayer rolls only once"
-    def choose_roll_hold(self):
-        if self.player_bank_score == 0:
-            return "roll"
-
-        else:
-            return "hold"
-
-
-
-class CautiousPlayer(Player):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    """Ensures Baseplayer rolls only once"""
 
     def choose_roll_hold(self):
         if self.player_bank_score == 0:
             return "roll"
-
         else:
             return "hold"
+
+
+class roll_five_then_hold(Player):
+    def choose_roll_hold(self):
+        if len(self.get_dice_values_per_role()) < 6:
+            return 'roll'
+        else:
+            return 'hold'
